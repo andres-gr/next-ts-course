5c9660eaf08efce3b8a363c53629b0b8ed51899c
Andres Garcia <andres.gar@gmail.com>
2019-10-25 15:30:58 -0500

    fixed alias issues in frontend

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const path = require('path');
-
-module.exports = {
-  webpack(config) {
-    config.resolve.alias['components'] = path.join(__dirname, 'components');
-    return config;
-  }
-};
