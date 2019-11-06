3ab4fc68939edc03df8738705e7608d72f1c1185
Andres Garcia <andres.gar@gmail.com>
2019-11-05 22:52:57 -0600

    module 3 video 14 done

--- backend/src/resolvers/Query.ts
+++ backend/src/resolvers/Query.ts
@@ -1,3 +1,9 @@
-const Query = {}
+import { forwardTo } from 'prisma-binding'
+import { QueryResolvers } from 'Resolvers/generated'
+
+const Query: QueryResolvers.Type = {
+  items : forwardTo('prisma'),
+  users : async (parent, args, { prismaClient }) => prismaClient.users(),
+}
 
 export default Query
